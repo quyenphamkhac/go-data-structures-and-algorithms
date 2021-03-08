76d40da914a1ef07062a9c16bc92bd95833460f9
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-08 14:09:11 +0700

    feat: check permutation v2 using hash table

--- search/permutation_list.go
+++ search/permutation_list.go
@@ -6,7 +6,7 @@ import (
 
 // Problems: Given two integer Lists. You have to check whether they are permutation of each other.
 
-// CheckPermutation use approach 1: sort and check
+// CheckPermutation solve problme use approach 1: sort and check
 func CheckPermutation(data1 []int, data2 []int) bool {
 	size1 := len(data1)
 	size2 := len(data2)
@@ -18,6 +18,31 @@ func CheckPermutation(data1 []int, data2 []int) bool {
 	sort.Ints(data2)
 	for i := 0; i < size1; i++ {
 		if data1[i] != data2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// CheckPermutationV2 solve problem use approach 2: hash-table
+// 1. Create a Hash-Table for all the elements of the first list.
+// 2. Traverse the other list from beginning to the end and search for each element in the Hash-Table.
+// 3. If all the elements are found in, the Hash-Table return true, otherwise return false.
+func CheckPermutationV2(data1 []int, data2 []int) bool {
+	size1 := len(data1)
+	size2 := len(data2)
+	if size1 != size2 {
+		return false
+	}
+
+	h := make(map[int]int)
+
+	for i := 0; i < size1; i++ {
+		h[data1[i]]++
+		h[data2[i]]--
+	}
+	for i := 0; i < size1; i++ {
+		if h[data1[i]] != 0 {
 			return false
 		}
 	}
--- search/permutation_list_test.go
+++ search/permutation_list_test.go
@@ -10,3 +10,12 @@ func TestCheckPermutation(t *testing.T) {
 		t.Errorf("Want True; but got %t", got)
 	}
 }
+
+func TestCheckPermutationV2(t *testing.T) {
+	data1 := []int{1, 2, 3, 4}
+	data2 := []int{3, 4, 2, 1}
+	got := CheckPermutationV2(data1, data2)
+	if got != true {
+		t.Errorf("Want True; but got %t", got)
+	}
+}
